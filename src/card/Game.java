c2cbfb79372dde4ae69b5b756dd98f192f2a52d1
riddham28 <riddhamavani7770@gmail.com>
2024-06-17 18:32:40 -0700

    Update Game.java

--- src/card/Game.java
+++ src/card/Game.java
@@ -1,44 +1,37 @@
 package card;
 
-
-
 public class Game {
+    private String name;
+    private Player player;
 
-	private String name;
-	private Player player;
+    public Game(String name, Player player) {
+        this.name = name;
+        this.player = player;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	/**
-	 * 
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Player getPlayer() {
-		return this.player;
-	}
+    public Player getPlayer() {
+        return this.player;
+    }
 
-	/**
-	 * 
-	 * @param player
-	 */
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 
-	public void play() {
-		// TODO - implement Game.play
-		throw new UnsupportedOperationException();
-	}
+    public void play() {
+        System.out.println("Playing the game...");
+        // Example play implementation
+    }
 
-	public void declareWinner() {
-		// TODO - implement Game.declareWinner
-		throw new UnsupportedOperationException();
-	}
-
+    public void declareWinner() {
+        System.out.println("Declaring the winner...");
+        // Example declare winner implementation
+    }
 }
